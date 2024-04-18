ee5d42cef5596e716738dd34ce23950c3a8357ec
DevelopmentCool2449 <163072436+DevelopmentCool2449@users.noreply.github.com>
2024-04-18 17:20:10 -0600

    Fix compatibility

--- sideline-eglot.el
+++ sideline-eglot.el
@@ -109,7 +109,11 @@ Argument COMMAND is required in sideline backend."
             (command (cl-getf matching-code-action :command))
             (server (eglot-current-server)))
          (sideline-eglot--inhibit-timeout
-           (eglot-execute server command)))))))
+          (if (fboundp #'eglot-execute)
+              (eglot-execute server command)
+            (eglot-execute-command server
+                                   (cl-getf command :command)
+                                   (cl-getf command :arguments)))))))))
 
 (provide 'sideline-eglot)
 ;;; sideline-eglot.el ends here
