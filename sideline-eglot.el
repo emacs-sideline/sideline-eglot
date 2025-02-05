01b5c962b4dfc1dd122448ebb7a093a642477958
Jen-Chieh Shen <jcs090218@gmail.com>
2025-02-05 15:43:35 -0800

    doc: Add documentary

--- sideline-eglot.el
+++ sideline-eglot.el
@@ -28,6 +28,14 @@
 ;;
 ;; Show eglot information with sideline.
 ;;
+;; 1) Add sideline-eglot to sideline backends list,
+;;
+;;   (setq sideline-backends-right '(sideline-eglot))
+;;
+;; 2) Then enable sideline-mode in the target buffer,
+;;
+;;   M-x sideline-mode
+;;
 
 ;;; Code:
 
